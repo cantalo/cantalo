c20e1c73a4e7f48f2c61472dc59f35f27c22779f
bbuhler <github@freiken-douhl.de>
2020-05-19 19:58:00 +0200

    Blacklisted localhost in Sentry configuration

--- src/client.js
+++ src/client.js
@@ -6,6 +6,7 @@ import './i18n.js';
 Sentry.init({
 	dsn: 'https://4793b8ca7f7a4478ab9b336dfa9142c9@o226409.ingest.sentry.io/5218450',
 	release: process.env.VERSION,
+	blacklistUrls: /localhost/,
 });
 
 sapper.start({
