d41db133ce9b1de7d221376328e863a017e2c834
bbuhler <github@freiken-douhl.de>
2020-06-19 17:39:33 +0200

    Updated Sentry, added additional log information

--- src/client.js
+++ src/client.js
@@ -1,5 +1,9 @@
 import * as Sentry from '@sentry/browser';
 import * as sapper from '@sapper/app';
+import { get as getStore } from 'svelte/store';
+
+import { time } from './stores/video';
+import { currentLine, currentSyllable } from './stores/song';
 
 import './i18n.js';
 
@@ -7,6 +11,29 @@ Sentry.init({
 	dsn: 'https://4793b8ca7f7a4478ab9b336dfa9142c9@o226409.ingest.sentry.io/5218450',
 	release: process.env.VERSION,
 	blacklistUrls: /localhost/,
+	beforeSend(event)
+	{
+		try
+		{
+			event.extra = event.extra || {};
+			event.context = event.context || {};
+
+			const $time = getStore(time);
+			if ($time) event.extra.playTime = $time;
+
+			const $currentLine = getStore(currentLine);
+			if ($currentLine) event.context.currentLine = $currentLine;
+
+			const $currentSyllable = getStore(currentSyllable);
+			if ($currentSyllable) event.context.currentSyllable = $currentSyllable;
+		}
+		catch (err)
+		{
+			console.error(err);
+		}
+
+		return event;
+	},
 });
 
 sapper.start({
