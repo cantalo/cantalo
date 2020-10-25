6814dab5425600d62e82651570b6f950e34bdafc
bbuhler <github@freiken-douhl.de>
2020-10-25 16:36:00 +0100

    Editor display different syllable types

--- src/components/editor/Syllable.svelte
+++ src/components/editor/Syllable.svelte
@@ -10,7 +10,7 @@
   {
     position: absolute;
     top: 80px;
-    bottom: 120px
+    bottom: 120px;
   }
 
   .syllable .pitch
@@ -21,23 +21,59 @@
     font-size: 12px;
     text-align: center;
     white-space: nowrap;
+    background: rgba(#A52A2A, .1);
+    border-left: 1px solid rgba(0,0,0, .1);
+    border-right: 1px solid rgba(0,0,0, .1);
   }
 
-  .syllable .pitch::before
+  .syllable .bar
+  {
+    position: absolute;
+    top: 100%;
+    width: 100%;
+  }
+
+  .syllable .bar::before
   {
     content: '';
     display: block;
     height: 15px;
     background: rgba(#A52A2A, .5);
     border-radius: 5px;
-    border: 1px solid brown;
+    border: 1px solid rgba(0,0,0, .6);
+  }
+
+  .syllable.freestyle .pitch
+  {
+    background: rgba(#ccc, .1);
+  }
+
+  .syllable.freestyle .bar::before
+  {
+    background: rgba(#ccc, .5);
+    border-style: dashed;
+  }
+
+  .syllable.golden .pitch
+  {
+    background: rgba(#DAA520, .1);
+  }
+
+  .syllable.golden .bar::before
+  {
+    background: rgba(#DAA520, .5);
   }
 </style>
 
-<div class="syllable" style="left: {100 / beats * syllable.start}%;
-                             width: {100 / beats * syllable.length}%">
+<div class="syllable" class:freestyle={syllable.type === 0} class:golden={syllable.type === 2}
+     style="left: {100 / beats * syllable.start}%;
+            width: {100 / beats * syllable.length}%">
   <div class="text">{syllable.text}</div>
-  <div class="pitch" style="top: {100 / 12 * (syllable.pitch % 12)}%">
-    {new Note(syllable.pitch).toString()}<sub>{Math.ceil(syllable.pitch / 12)}</sub>
+  <div class="pitch" style="height: {syllable.type === 0 ? 10 : 100 / 12 * (syllable.pitch % 12)}%">
+    <div class="bar">
+      {#if syllable.type > 0}
+        {new Note(syllable.pitch).toString()}<sub>{Math.ceil(syllable.pitch / 12)}</sub>
+      {/if}
+    </div>
   </div>
 </div>
