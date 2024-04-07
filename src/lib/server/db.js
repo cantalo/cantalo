71730aab2ceb03aa2ab6622c211a9e2d1bfa7544
Benjamin Buhler <github@freiken-douhl.de>
2024-04-07 13:56:21 +0200

    fix: re-added song shuffle

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,8 +1,20 @@
 import { env } from '$env/dynamic/private';
 
+function shuffle(array) {
+  let currentIndex = array.length;
+  while (currentIndex !== 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
 export async function getSongs() {
   const response = await fetch(env.SONG_SERVICE);
-  return response.json();
+  const result = await response.json();
+  shuffle(result);
+  return result;
 }
 
 export async function getSong(id) {
