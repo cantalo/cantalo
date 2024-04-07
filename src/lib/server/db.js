022e934f9a4f8308aa62ba03fa0f2aeed741d8bc
Benjamin Buhler <github@freiken-douhl.de>
2024-04-07 13:35:06 +0200

    refactor: use cantalo song service as datasource

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,29 +1,11 @@
-import lowDB from 'lowdb';
-import FileAsync from 'lowdb/adapters/FileAsync.js';
-
-const adapter = new FileAsync(process.env['DB_PATH'], {
-  serialize: obj => JSON.stringify(obj),
-});
-
-export default async function DB()
-{
-  const db = await lowDB(adapter);
-  return db.defaults({ songs: [] })
-}
+import { env } from '$env/dynamic/private';
 
 export async function getSongs() {
-  const db = await DB();
-
-  return db.get('songs')
-    .map(it => it.meta)
-    .shuffle()
-    .value();
+  const response = await fetch(env.SONG_SERVICE);
+  return response.json();
 }
 
 export async function getSong(id) {
-  const db = await DB();
-
-  return db.get('songs')
-    .find({ id })
-    .value();
+  const response = await fetch(`${env.SONG_SERVICE}/${id}.json`);
+  return response.json();
 }
